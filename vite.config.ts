7e29ff393ffb5fc703d21d49d9d4eb2e2829c1d8
Mateus De Nardo <m.nardo@epilot.cloud>
2022-10-24 15:38:40 +0200

    fix assets location for gh pages

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { defineConfig } from "vite";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/yama-arashi/",
   resolve: {
     alias: {
       "~bootstrap": path.resolve(__dirname, "node_modules/bootstrap"),
